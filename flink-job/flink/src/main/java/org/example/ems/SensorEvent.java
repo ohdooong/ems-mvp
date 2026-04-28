6b94ae293f328a8fa2a14b77bb782272a5858744
오승환 <ohseung1486@gmail.com>
2026-04-28 20:46:28 +0900

    Feat: Flink Job Commit

--- flink-job/flink/src/main/java/org/example/ems/SensorEvent.java
+++ flink-job/flink/src/main/java/org/example/ems/SensorEvent.java
@@ -3,6 +3,9 @@ package org.example.ems;
 
 import java.time.Instant;
 
+/**
+ * The type Sensor event.
+ */
 public class SensorEvent {
     private String deviceId;
     private String siteId;
@@ -12,9 +15,23 @@ public class SensorEvent {
     private Instant eventTime;
     private Instant ingestionTime;
 
+    /**
+     * Instantiates a new Sensor event.
+     */
     public SensorEvent() {
     }
 
+    /**
+     * Instantiates a new Sensor event.
+     *
+     * @param deviceId      the device id
+     * @param siteId        the site id
+     * @param zoneId        the zone id
+     * @param powerUsage    the power usage
+     * @param status        the status
+     * @param eventTime     the event time
+     * @param ingestionTime the ingestion time
+     */
     public SensorEvent(String deviceId, String siteId, String zoneId, double powerUsage, String status, Instant eventTime, Instant ingestionTime) {
         this.deviceId = deviceId;
         this.siteId = siteId;
@@ -25,62 +42,137 @@ public class SensorEvent {
         this.ingestionTime = ingestionTime;
     }
 
+    /**
+     * Gets device id.
+     *
+     * @return the device id
+     */
     public String getDeviceId() {
         return deviceId;
     }
 
+    /**
+     * Sets device id.
+     *
+     * @param deviceId the device id
+     */
     public void setDeviceId(String deviceId) {
         this.deviceId = deviceId;
     }
 
+    /**
+     * Gets site id.
+     *
+     * @return the site id
+     */
     public String getSiteId() {
         return siteId;
     }
 
+    /**
+     * Sets site id.
+     *
+     * @param siteId the site id
+     */
     public void setSiteId(String siteId) {
         this.siteId = siteId;
     }
 
+    /**
+     * Gets zone id.
+     *
+     * @return the zone id
+     */
     public String getZoneId() {
         return zoneId;
     }
 
+    /**
+     * Sets zone id.
+     *
+     * @param zoneId the zone id
+     */
     public void setZoneId(String zoneId) {
         this.zoneId = zoneId;
     }
 
+    /**
+     * Gets power usage.
+     *
+     * @return the power usage
+     */
     public double getPowerUsage() {
         return powerUsage;
     }
 
+    /**
+     * Sets power usage.
+     *
+     * @param powerUsage the power usage
+     */
     public void setPowerUsage(double powerUsage) {
         this.powerUsage = powerUsage;
     }
 
+    /**
+     * Gets status.
+     *
+     * @return the status
+     */
     public String getStatus() {
         return status;
     }
 
+    /**
+     * Sets status.
+     *
+     * @param status the status
+     */
     public void setStatus(String status) {
         this.status = status;
     }
 
+    /**
+     * Gets event time.
+     *
+     * @return the event time
+     */
     public Instant getEventTime() {
         return eventTime;
     }
 
+    /**
+     * Sets event time.
+     *
+     * @param eventTime the event time
+     */
     public void setEventTime(Instant eventTime) {
         this.eventTime = eventTime;
     }
 
+    /**
+     * Gets ingestion time.
+     *
+     * @return the ingestion time
+     */
     public Instant getIngestionTime() {
         return ingestionTime;
     }
 
+    /**
+     * Sets ingestion time.
+     *
+     * @param ingestionTime the ingestion time
+     */
     public void setIngestionTime(Instant ingestionTime) {
         this.ingestionTime = ingestionTime;
     }
 
+    /**
+     * To string string.
+     *
+     * @return the string
+     */
     @Override
     public String toString() {
         return "SensorEvent{" +
--- flink-job/flink/src/main/java/org/example/ems/SensorLatestJob.java
+++ flink-job/flink/src/main/java/org/example/ems/SensorLatestJob.java
@@ -1,7 +1,38 @@
 package org.example.ems;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.connector.kafka.source.KafkaSource;
+import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+/**
+ * The type Sensor latest job.
+ */
 public class SensorLatestJob {
+
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     * @throws Exception the exception
+     */
     public static void main(String[] args) throws Exception {
-        
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        // Source 설정
+        KafkaSource<String> kafkaSource = KafkaSource.<String>builder()
+            .setBootstrapServers("kafka:9092")  // 부트스트랩서버 설정
+            .setTopics("raw.sensor.energy")     // 토픽
+            .setGroupId("ems-sensor-latest-job")    // GroupId 설정
+            .setStartingOffsets(OffsetsInitializer.earliest())  //
+            .setValueOnlyDeserializer(new SimpleStringSchema())
+            .build();
+
+
+
     }
 }
