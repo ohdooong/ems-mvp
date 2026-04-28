9a5c8923a109655c375c7e2ef2850392255d2402
ohdooong <ohseung1486@gmail.com>
2026-04-28 22:25:56 +0900

    Feat: flink-job 코드 추가 (Sensor Latest Job)

--- flink-job/flink/src/main/java/org/example/ems/SensorLatestJob.java
+++ flink-job/flink/src/main/java/org/example/ems/SensorLatestJob.java
@@ -1,10 +1,20 @@
 package org.example.ems;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.connector.jdbc.JdbcConnectionOptions;
+import org.apache.flink.connector.jdbc.JdbcExecutionOptions;
+import org.apache.flink.connector.jdbc.JdbcSink;
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.Objects;
 
 /**
  * The type Sensor latest job.
@@ -25,14 +35,84 @@ public class SensorLatestJob {
 
         // Source 설정
         KafkaSource<String> kafkaSource = KafkaSource.<String>builder()
-            .setBootstrapServers("kafka:9092")  // 부트스트랩서버 설정
+            .setBootstrapServers("kafka:9092")  // 부트스트랩서버 설정 -> 도커 컨테이너 도메인
             .setTopics("raw.sensor.energy")     // 토픽
-            .setGroupId("ems-sensor-latest-job")    // GroupId 설정
-            .setStartingOffsets(OffsetsInitializer.earliest())  //
+            .setGroupId("ems-sensor-latest-job")    // Consumer GroupId 설정
+            .setStartingOffsets(OffsetsInitializer.earliest())  // Kafka메시지를 어디서부터 읽을건지 결정 earliest는 가장 오래된 메세지 부터 읽음
             .setValueOnlyDeserializer(new SimpleStringSchema())
             .build();
 
+        env.fromSource(kafkaSource, WatermarkStrategy.noWatermarks(), "Kafka Source")
+                .map(SensorLatestJob::parseJson)
+                .filter(Objects::nonNull)
+                .returns(SensorEvent.class) // 타입 힌트
+                .addSink(
+                        JdbcSink.sink(
+                                """
+                                    INSERT INTO sensor_latest
+                                    (
+                                        device_id,
+                                        site_id,
+                                        zone_id,
+                                        power_usage,
+                                        status,
+                                        event_time,
+                                        ingestion_time,
+                                        updated_at
+                                    )
+                                    VALUES(?, ?, ?, ?, ?, ?, ?, now())
+                                    ON CONFLICT (device_id)
+                                    DO UPDATE SET
+                                        site_id = EXCLUDED.site_id,
+                                        zone_id = EXCLUDED.zone_id,
+                                        power_usage = EXCLUDED.power_usage,
+                                        status = EXCLUDED.status,
+                                        event_time = EXCLUDED.event_time,
+                                        ingestion_time = EXCLUDED.ingestion_time,
+                                        updated_at = CURRENT_TIMESTAMP
+                                        """,
+                                (ps, event) -> {
+                                    ps.setString(1, event.getDeviceId());
+                                    ps.setString(2, event.getSiteId());
+                                    ps.setString(3, event.getZoneId());
+                                    ps.setDouble(4, event.getPowerUsage());
+                                    ps.setString(5, event.getStatus());
+                                    ps.setTimestamp(6, Timestamp.from(event.getEventTime()));
+                                    ps.setTimestamp(7, Timestamp.from(event.getIngestionTime()));
+                                },
+                                JdbcExecutionOptions.builder()
+                                        .withBatchSize(100)
+                                        .withBatchIntervalMs(1000)
+                                        .withMaxRetries(3)
+                                        .build(),
+                                new JdbcConnectionOptions.JdbcConnectionOptionsBuilder()
+                                        .withUrl("jdbc:postgresql://postgres:5432/ems")
+                                        .withDriverName("org.postgresql.Driver")
+                                        .withUsername("ems_user")
+                                        .withPassword("ems_pass")
+                                        .build()
+                        )
+                );
 
+        // Job 시작
+        env.execute("EMS Sensor Latest Job");
+    }
 
+    private static SensorEvent parseJson(String raw) {
+        try {
+            JsonNode jsonNode = objectMapper.readTree(raw);
+            return new SensorEvent(
+                    jsonNode.get("device_id").asText(),
+                    jsonNode.get("site_id").asText(),
+                    jsonNode.get("zone_id").asText(),
+                    jsonNode.get("power_usage").asDouble(),
+                    jsonNode.get("status").asText(),
+                    Instant.parse(jsonNode.get("event_time").asText()),
+                    Instant.parse(jsonNode.get("ingestion_time").asText())
+            );
+        } catch (Exception e) {
+            System.out.println("Failed to parse event: " + raw);
+            return null;
+        }
     }
 }
